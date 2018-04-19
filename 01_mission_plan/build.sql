299256edb9c4a328102e909659d05afa85124f69
Scott Sargent <scott.sargent@gmail.com>
2018-04-19 07:29:06 -0400

    creating build script

---
+++ 01_mission_plan/build.sql
@@ -0,0 +1,18 @@
+create schema if not exists import;
+drop table if exists import.master_plan;
+create table import.master_plan (
+	start_time_utc text,
+	duration text,
+	date text,
+	team text,
+	spass_type text,
+	target text,
+	request_name text,
+	library_definition text,
+	title text,
+	description text
+);
+
+copy import.master_plan
+from '/home/scott/data/curiousmoon/data/master_plan.csv'
+with delimiter ',' header csv;
